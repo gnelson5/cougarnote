94d0e3c7b553851617a58c4f936c9d9def091acb
Garrett Nelson <gnelson5@student.cscc.edu>
2023-10-31 18:37:29 -0400

    Add links to individual folder routes

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -10,8 +10,8 @@
       @if (count($notes) > 0)
       <ul class="space-y-4">
         @foreach ($notes as $note)
-        <li class="p-6 bg-white dark:bg-gray-800 sm:rounded-lg shadow space-y-2">
-          <div class="flex gap-4 justify-between items-center">
+        <li class="p-6 bg-white dark:bg-gray-800 sm:rounded-lg shadow space-y-3">
+          <div class="flex gap-4 justify-between items-start">
             <a href="/notes/{{ $note->id }}" class="text-lg font-medium hover:text-black dark:hover:text-white outline-none focus-visible:text-black dark:focus-visible:text-white underline-offset-2 hover:underline focus-visible:underline">
               <h3>{{ $note->title }}</h3>
             </a>
@@ -25,10 +25,13 @@
             </form>
           </div>
           @if (!empty($note->folder))
-          <p>Folder: <span class="font-semibold">{{ $note->folder->name }}</span></p>
+          <p>
+            <span class="text-sm text-gray-600 dark:text-gray-400">Folder:</span>
+            <a href="/folders/{{ $note->folder->id }}" class="font-medium hover:text-black dark:hover:text-white outline-none focus-visible:text-black dark:focus-visible:text-white underline-offset-2 hover:underline focus-visible:underline">{{ $note->folder->name }}</a>
+          </p>
           @endif
           <p class="text-gray-800 dark:text-gray-200 line-clamp-4">{{ $note->body }}</p>
-          <p class="font-light text-gray-700 dark:text-gray-300">{{ date_format($note->updated_at, 'n-j-Y') }}</p>
+          <p class="text-sm text-gray-600 dark:text-gray-400">{{ date_format($note->updated_at, 'n-j-Y') }}</p>
         </li>
         @endforeach
       </ul>
