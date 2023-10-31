183d26b9884d6d99e11cce21fc8e66f73ee82b29
Garrett Nelson <gnelson5@student.cscc.edu>
2023-10-31 13:06:37 -0400

    Add comments to notes controller

--- app/Http/Controllers/NotesController.php
+++ app/Http/Controllers/NotesController.php
@@ -8,11 +8,17 @@ use Illuminate\Support\Facades\Auth;
 
 class NotesController extends Controller
 {
+  /**
+   * Get all notes for the current user.
+   */
   public function index()
   {
     return view('dashboard', ['notes' => Note::where('user_id', Auth::id())->with('user')->with('folder')->get()]);
   }
 
+  /**
+   * Delete the specified note.
+   */
   public function destroy(Note $note)
   {
     $note->delete();
