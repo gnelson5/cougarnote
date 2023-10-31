d91943cdea9f6ac9343f39fefe8e8d1be596a8b5
Garrett Nelson <gnelson5@student.cscc.edu>
2023-10-31 11:26:32 -0400

    Display logged in user's notes on dashboard

---
+++ app/Http/Controllers/NotesController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Note;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class NotesController extends Controller
+{
+  public function index()
+  {
+    return view('dashboard', ['notes' => Note::where('user_id', Auth::id())->with('user')->get()]);
+  }
+}
--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,17 +1,21 @@
 <x-app-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
-            {{ __('Dashboard') }}
-        </h2>
-    </x-slot>
+  <x-slot name="header">
+    <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
+      {{ __('Notes') }}
+    </h2>
+  </x-slot>
 
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900 dark:text-gray-100">
-                    {{ __("You're logged in!") }}
-                </div>
-            </div>
-        </div>
+  <div class="py-12">
+    <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+      <ul class="space-y-4">
+        @foreach ($notes as $note)
+        <li class="p-6 bg-white dark:bg-gray-800 sm:rounded-lg shadow space-y-1">
+          <p class="text-lg font-medium">{{ $note->title }}</p>
+          <p class="text-gray-800 dark:text-gray-200 line-clamp-4">{{ $note->body }}</p>
+          <p class="font-light text-gray-700 dark:text-gray-300">{{ date_format($note->updated_at, 'n-j-Y') }}</p>
+        </li>
+        @endforeach
+      </ul>
     </div>
+  </div>
 </x-app-layout>
